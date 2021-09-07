fefee934a476086267d9eb2b842b1ccc56969045
Deepali Jain <deepali.jain.1980@gmail.com>
2021-09-07 17:29:47 +0530

    second commit

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.get('/', (req, res) => {
 });
 
 
-
 /*-------------------------------------------------------------------------------*/
 //  Post method To create new user with prisma 
 //   http://localhost:3000/api/users
@@ -102,7 +101,6 @@ app.post("/api/users/:id/exercises", (req,res)=>{
   if(!(req.body.description && req.body.duration && req.params.id))
   {
     return res.send({error:"Necessary Fields are required"});
-
   }
         
   prisma.user.findUnique({
@@ -143,10 +141,6 @@ app.post("/api/users/:id/exercises", (req,res)=>{
         })
   })
   .catch(err=>console.log(err));
-
-  
-        
-
 });
 
 
@@ -175,60 +169,64 @@ app.post("/api/users/:id/exercises", (req,res)=>{
 */
 app.get("/api/users/:id/logs", (req, res) => {
   let userId=req.params.id;
-  let fromD=req.query.from;
-  let to=req.query.to;
-  let limit=req.query.limit;
+  let fromD=req.query.from?new Date(req.query.from):new Date(0);
+  let to=req.query.to?new Date(req.query.to):new Date(Date.parse(new Date()) + 8640000*30*12);
+  let limit=req.query.limit?+req.query.limit:1000;
 
-prisma.user.findUnique({
-  where: {
-    id: userId,
-  },
-  include: {
-    exercises: {
-      orderBy: {
-        date: 'asc',
+  prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      exercises: {
+        select:{
+          description: true,
+          duration:true,
+          date:true
+        },
+        where:{
+          AND: [
+            {
+              date: {
+                gte: fromD,
+              },
+            },
+            {
+              date: {
+                lte: to,
+              },
+            },
+          ],
+        },
+        orderBy: {
+          date: 'asc',
+              },
+        take:limit
       },
-  },
-}})
-.then(data=>{
-  if(!data){
-    return res.send({
-      error:"User with this id does not exist"})
-  }
-    
-    var exercises = data.exercises;
-    var log = exercises.map(item=>{
-            return{description:item.description, 
+  }})
+  .then(data=>{
+    if(!data){
+      return res.send({
+        error:"User with this id does not exist"})
+    }
+
+    var log=data.exercises.map(item=>{
+      return{description:item.description, 
             duration:+item.duration,
-            date:item.date.toDateString()
-     }
-   });
-    if(fromD) {
-    var fromDate = Date.parse(new Date(fromD).toUTCString());
-    log = log.filter(item => 
-      Date.parse(new Date(item.date).toUTCString()) >= fromDate
-              );
-    }
-    if(to) {
-    var toDate = Date.parse(new Date(to).toUTCString());
-    log = log.filter(item => 
-      Date.parse(new Date(item.date).toUTCString()) <= toDate);
-    }
-    if(limit) {
-    log = log.slice(0, +limit);
-    }
+            date:new Date(item.date).toDateString()
+            }
+
+    });
+
     res.send({
-            username:data.username, 
-            count:log.length, 
-            id:data.id, 
-            log:log
-          })
-})
-.catch(err=>console.log(err));
-
+              username:data.username, 
+              count:data.exercises.length, 
+              id:data.id,
+              log:log
+            })
+  })
+  .catch(err=>console.log(err));
 });//end of function
-
-
 
 
 
